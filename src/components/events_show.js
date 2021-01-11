f4b358ace3b9e2b47fdc0b603f59a07fe55eb57b
D-Igarashi <iga7agi@gmail.com>
2021-01-11 23:25:23 +0900

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,13 +3,20 @@ import { connect } from 'react-redux'
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom'
 
-import { postEvent } from '../actions'
+import { getEvent, deleteEvent, putEvent } from '../actions'
 
-class EventsNew extends Component {
+class EventsShow extends Component {
 	constructor(props) {
 		super(props)
 		this.onSubmit = this.onSubmit.bind(this)
+		this.onDeleteClick = this.onDeleteClick.bind(this)
+
 	}
+	componentDidMount() {
+		const { id } = this.props.match.params
+		if (id) this.props.getEvent(id)
+	}
+
 	renderField(field) {
 		const { input, label, type, meta: { touched, error } } = field
 		return (
@@ -19,8 +26,15 @@ class EventsNew extends Component {
 			</div>)
 	}
 
+	async onDeleteClick(values) {
+
+		const { id } = this.props.match.params
+		await this.props.deleteEvent(id)
+		this.props.history.push('/')
+	}
+
 	async onSubmit(values) {
-		await this.props.postEvent(values)
+		await this.props.putEvent(values)
 		this.props.history.push('/')
 	}
 
@@ -34,6 +48,7 @@ class EventsNew extends Component {
 				</div>
 				<input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
 				<Link to="/" > Cansel</Link>
+				<Link to="/" onClick={this.onDeleteClick}>Delete</Link>
 			</form>
 		)
 	}
@@ -50,8 +65,12 @@ const validate = values => {
 }
 
 
-const mapDispatchToProps = ({ postEvent })
+const mapDispatchToProps = ({ deleteEvent, getEvent, putEvent })
+const mapStateToProps = (state, ownProps) => {
+	const event = state.events[ownProps.match.params.id]
+	return { initialValues: event, state }
+}
 
-export default connect(null, mapDispatchToProps)(
-	reduxForm({ validate, form: 'eventNewform' })(EventsNew)
+export default connect(mapStateToProps, mapDispatchToProps)(
+	reduxForm({ validate, form: 'eventShowForm', enebleReinitialize: true })(EventsShow)
 )
--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,11 +1,25 @@
 import _ from 'lodash'
-import { READ_EVENTS } from '../actions'
+import {
+    READ_EVENTS,
+    DELETE_EVENT,
+    READ_EVENT,
+    UPDATE_EVENT,
+    CREATE_EVENT
+} from '../actions'
 
 // eslint-disable-next-line
 export default (events = {}, action) => {
     switch (action.type) {
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id')
+        case READ_EVENT:
+        case UPDATE_EVENT:
+        case CREATE_EVENT:
+            const data = action.response.data
+            return { ...events, [data.id]: data }
+        case DELETE_EVENT:
+            delete events[action.id]
+            return { ...events }
         default:
             return events
     }
